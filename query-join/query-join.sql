6625ac6025c3f0a512d9fb4e43d996d7b93918e6
italocirillo <italocirillo@gmail.com>
2023-05-16 16:39:47 +0200

    ESERCIZIO COMPLETO + BONUS

--- query-join/query-join.sql
+++ query-join/query-join.sql
@@ -29,9 +29,11 @@
 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 
     SELECT `students`.`surname`,`students`.`name`,`degrees`.`name` AS `name_degrees`,`departments`.`name` AS `name_departments` FROM `students` 
-    JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id` 
-    JOIN `departments` ON `degrees`.`department_id` = `departments`.`id` 
-    ORDER BY `students`.`surname` ASC;
+    JOIN `degrees` 
+    ON `students`.`degree_id` = `degrees`.`id` 
+    JOIN `departments` 
+    ON `degrees`.`department_id` = `departments`.`id` 
+    ORDER BY `students`.`surname`,`students`.`name` ASC;
 
 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
@@ -61,3 +63,15 @@
 
 
 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+
+    SELECT`students`.`name`,`students`.`surname`,`courses`.`name` AS `courses`,
+    COUNT(*) AS `exam_appeal`,MAX(`vote`) AS `max_vote`
+    FROM `students` 
+    JOIN `exam_student`
+    ON `students`.`id` = `exam_student`.`student_id`
+    JOIN `exams`
+    ON `exam_student`.`exam_id` = `exams`.`id`
+    JOIN `courses`
+    ON `exams`.`course_id` = `courses`.`id`
+    GROUP BY `courses`.`id`,`students`.`id`
+    HAVING `max_vote` >= 18
